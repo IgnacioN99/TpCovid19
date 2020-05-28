49503a9b663ef72dd326ec06a2cc7994520db3ed
Ignacio Nicolas Flores <inifl99@gmail.com>
2020-05-28 18:35:42 -0300

    Update PuntoC.m

--- PuntoC.m
+++ PuntoC.m
@@ -20,200 +20,200 @@ ylabel('Muertos');
 grid on
 hold off;
 
-###Infectados
-##y=xlsread('covid19.xlsx','Argentina','B2:B11')';
-##printf("An de Infectados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Infectados','Curva de ajuste de grado 4','location','northwest');
-##title('Infectados Argentina');
-##xlabel('Dias');
-##ylabel('Infectados');
-##grid on;
-##hold off;
-##
-###Recuperados
-##y=xlsread('covid19.xlsx','Argentina','D2:D11')';
-##printf("An de Recuperados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
-##title('Recuperados Argentina');
-##xlabel('Dias');
-##ylabel('Recuperados');
-##grid on
-##hold off;
-##
-##
-##printf("Chile \n");
-##
-###Chile
-##
-###Muertes
-##y=xlsread('covid19.xlsx','Chile','C2:C11')';
-##printf("An de Muertes\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rx','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Muertos','Curva de ajuste de grado 4','location','northwest');
-##title('Muertes Chile');
-##xlabel('Dias');
-##ylabel('Muertos');
-##grid on
-##hold off;
-##
-###Infectados
-##y=xlsread('covid19.xlsx','Chile','B2:B11')';
-##printf("An de Infectados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Infectados','Curva de ajuste de grado 4','location','northwest');
-##title('Infectados Chile');
-##xlabel('Dias');
-##ylabel('Infectados');
-##grid on;
-##hold off;
-##
-###Recuperados
-##y=xlsread('covid19.xlsx','Chile','D2:D11')';
-##printf("An de Recuperados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
-##title('Recuperados Chile');
-##xlabel('Dias');
-##ylabel('Recuperados');
-##grid on
-##hold off;
-##
-##
-###Brasil
-##printf("Brasil\n");
-###Muertes
-##y=xlsread('covid19.xlsx','Brasil','C2:C11')';
-##printf("An de Muertes\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rx','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Muertos','Curva de ajuste de grado 4','location','northwest');
-##title('Muertes Brasil');
-##xlabel('Dias');
-##ylabel('Muertos');
-##grid on
-##hold off;
-##
-###Infectados
-##y=xlsread('covid19.xlsx','Brasil','B2:B11')';
-##printf("An de Infectados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Infectados','Curva de ajuste de grado 4','location','northwest');
-##title('Infectados Brasil');
-##xlabel('Dias');
-##ylabel('Infectados');
-##grid on;
-##hold off;
-##
-###Recuperados
-##y=xlsread('covid19.xlsx','Brasil','D2:D11')';
-##printf("An de Recuperados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
-##title('Recuperados Brasil');
-##xlabel('Dias');
-##ylabel('Recuperados');
-##grid on
-##hold off;
-##
-##
-###Suiza
-##printf("Suiza\n")
-###Muertes
-##y=xlsread('covid19.xlsx','Suiza','C2:C11')';
-##printf("An de Muertes\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rx','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Muertos','Curva de ajuste de grado 4','location','northwest');
-##title('Muertes Suiza');
-##xlabel('Dias');
-##ylabel('Muertos');
-##grid on
-##hold off;
-##
-###Infectados
-##y=xlsread('covid19.xlsx','Suiza','B2:B11')';
-##printf("An de Infectados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Infectados','Curva de ajuste de grado 4','location','northwest');
-##title('Infectados Suiza');
-##xlabel('Dias');
-##ylabel('Infectados');
-##grid on;
-##hold off;
-##
-###Recuperados
-##y=xlsread('covid19.xlsx','Suiza','D2:D11')';
-##printf("An de Recuperados\n");
-##a=polyfit(dias,y,4)
-##figure;
-##plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
-##z=@(dias) polyval(a,dias);
-##xticks(dias);
-##xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
-##fplot(z,[dias(1),dias(end)],'b-'),hold on;
-##legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
-##title('Recuperados Suiza');
-##xlabel('Dias');
-##ylabel('Recuperados');
-##grid on
-##hold off;
+#Infectados
+y=xlsread('covid19.xlsx','Argentina','B2:B11')';
+printf("An de Infectados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Infectados','Curva de ajuste de grado 4','location','northwest');
+title('Infectados Argentina');
+xlabel('Dias');
+ylabel('Infectados');
+grid on;
+hold off;
+
+#Recuperados
+y=xlsread('covid19.xlsx','Argentina','D2:D11')';
+printf("An de Recuperados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
+title('Recuperados Argentina');
+xlabel('Dias');
+ylabel('Recuperados');
+grid on
+hold off;
+
+
+printf("Chile \n");
+
+#Chile
+
+#Muertes
+y=xlsread('covid19.xlsx','Chile','C2:C11')';
+printf("An de Muertes\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rx','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Muertos','Curva de ajuste de grado 4','location','northwest');
+title('Muertes Chile');
+xlabel('Dias');
+ylabel('Muertos');
+grid on
+hold off;
+
+#Infectados
+y=xlsread('covid19.xlsx','Chile','B2:B11')';
+printf("An de Infectados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Infectados','Curva de ajuste de grado 4','location','northwest');
+title('Infectados Chile');
+xlabel('Dias');
+ylabel('Infectados');
+grid on;
+hold off;
+
+#Recuperados
+y=xlsread('covid19.xlsx','Chile','D2:D11')';
+printf("An de Recuperados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
+title('Recuperados Chile');
+xlabel('Dias');
+ylabel('Recuperados');
+grid on
+hold off;
+
+
+#Brasil
+printf("Brasil\n");
+#Muertes
+y=xlsread('covid19.xlsx','Brasil','C2:C11')';
+printf("An de Muertes\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rx','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Muertos','Curva de ajuste de grado 4','location','northwest');
+title('Muertes Brasil');
+xlabel('Dias');
+ylabel('Muertos');
+grid on
+hold off;
+
+#Infectados
+y=xlsread('covid19.xlsx','Brasil','B2:B11')';
+printf("An de Infectados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Infectados','Curva de ajuste de grado 4','location','northwest');
+title('Infectados Brasil');
+xlabel('Dias');
+ylabel('Infectados');
+grid on;
+hold off;
+
+#Recuperados
+y=xlsread('covid19.xlsx','Brasil','D2:D11')';
+printf("An de Recuperados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
+title('Recuperados Brasil');
+xlabel('Dias');
+ylabel('Recuperados');
+grid on
+hold off;
+
+
+#Suiza
+printf("Suiza\n")
+#Muertes
+y=xlsread('covid19.xlsx','Suiza','C2:C11')';
+printf("An de Muertes\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rx','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Muertos','Curva de ajuste de grado 4','location','northwest');
+title('Muertes Suiza');
+xlabel('Dias');
+ylabel('Muertos');
+grid on
+hold off;
+
+#Infectados
+y=xlsread('covid19.xlsx','Suiza','B2:B11')';
+printf("An de Infectados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'rs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Infectados','Curva de ajuste de grado 4','location','northwest');
+title('Infectados Suiza');
+xlabel('Dias');
+ylabel('Infectados');
+grid on;
+hold off;
+
+#Recuperados
+y=xlsread('covid19.xlsx','Suiza','D2:D11')';
+printf("An de Recuperados\n");
+a=polyfit(dias,y,4)
+figure;
+plot(dias,y,'gs','markersize',4,'markerfacecolor','r'), hold on;
+z=@(dias) polyval(a,dias);
+xticks(dias);
+xticklabels({'28/03','30/03','31/03','03/04','05/04','07/04','09/04','17/04','01/05','03/05'});
+fplot(z,[dias(1),dias(end)],'b-'),hold on;
+legend('Recuperados','Curva de ajuste de grado 4','location','northwest');
+title('Recuperados Suiza');
+xlabel('Dias');
+ylabel('Recuperados');
+grid on
+hold off;
